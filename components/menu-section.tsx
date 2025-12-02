ac514bbac40aac9d8246acc8da2d5389c3eeca57
Jnyananjan Sarkar <jnyananjansarkar01@gmail.com>
2025-12-02 12:05:58 +0530

    Update menu-section.tsx

--- components/menu-section.tsx
+++ components/menu-section.tsx
@@ -16,7 +16,7 @@ export default function MenuSection({ category, onBack }: MenuSectionProps) {
     rice: "Rice Items",
     chicken: "Chicken Main Course",
     mutton: "Mutton Main Course",
-    tandoori-chicken: "Tandoori Chicken",
+    tandoori: "Tandoori Chicken",
     seafood: "Seafood",
     tandoori: "Tandoori",
     noodles: "Noodles",
